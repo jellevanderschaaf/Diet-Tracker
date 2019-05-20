9fc76afd53e65167818f714ac89c1215298d163b
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-05-20 09:22:57 +0200

    Created objects to store data for my first three food items.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,32 @@
 
+// Food Items objects
+
+var mincedBeef = {
+    fat: 72,
+    carbs: 1.8,
+    protein: 120,
+    kcals: 1140,
+    price: 6.4,
+};
+
+var egg = {
+    fat: 6.7,
+    carbs: 0.9,
+    protein: 7.7,
+    kcals: 95,
+    price: 0.164,
+};
+
+var butter = {
+    fat: 82.5,
+    carbs: 0.7,
+    protein: 0.7,
+    kcals: 748,
+    price: 1.196,
+};
+
+//
+
 function addFoodItemToList(value) {
     document.getElementById("listToday").innerHTML += value + "<br />";
 
