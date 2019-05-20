b26a167478c8de41b5f5e501cf8c3af1fb533750
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-05-20 15:45:06 +0200

    Got totals for Fats, Carbs, Protein and Costs working.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 
 var totalKcals = [];
+var totalFats = [];
+var totalCarbs = [];
+var totalProtein = [];
+var totalCosts = [];
 
 // Food Items objects
 
@@ -35,12 +39,21 @@ var butter = {
 function addFoodItemToList(value) {
     var food = eval(value);
     document.getElementById("listToday").innerHTML += food.name + "<br />" + "fat: " + food.fat + " grams carbs: " + food.carbs + " grams protein: " + food.protein + " grams kcals: " + food.kcals + " costs: " + food.price + "<br />";
+    
     totalKcals.push(food.kcals);
+    totalFats.push(food.fat);
+    totalCarbs.push(food.carbs);
+    totalProtein.push(food.protein);
+    totalCosts.push(food.price);
 
     function getSum(total, num) {
         return total + num;
-  }
+    }
    {
     document.getElementById("totalKcal").innerHTML = totalKcals.reduce(getSum);
+    document.getElementById("totalFats").innerHTML = totalFats.reduce(getSum);
+    document.getElementById("totalCarbs").innerHTML = totalCarbs.reduce(getSum);
+    document.getElementById("totalProtein").innerHTML = totalProtein.reduce(getSum);
+    document.getElementById("totalCosts").innerHTML = totalCosts.reduce(getSum);
   }
 }
