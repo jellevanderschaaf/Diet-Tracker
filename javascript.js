9a8f16b33d86c71ef09154db3f3df9e638f3d974
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-06-08 10:08:33 +0200

    Added id's to the food items objects.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 // Global variables
 
+var idCounter = 10;
 var valueRemove;
 
 var totalKcals = [];
@@ -16,8 +17,11 @@ var totalCostsSum;
 
 // Food Items objects
 
+let foodItems = [];
+
 class FoodItem {
-    constructor(name, fat, carbs, protein, kcals, price) {
+    constructor(id, name, fat, carbs, protein, kcals, price) {
+        this.id = id;
         this.name = name;
         this.fat = fat;
         this.carbs = carbs;
@@ -27,21 +31,22 @@ class FoodItem {
     }
 }
 
-var mincedBeef = new FoodItem('minced beef (600 grams)', 72, 1.8, 120, 1140, 6.4);
-var egg = new FoodItem('egg (1x)', 6.7, 0.9, 7.7, 95, 0.164);
-var butter = new FoodItem('butter (100 grams)', 82.5, 0.7, 0.7, 748, 1.196);
-var chickenLegg = new FoodItem('chicken legg (300 grams)', 39, 0, 54, 570, 1.22);
-var mackerel = new FoodItem('mackerel (350 grams)', 63, 0, 77, 875, 3.32);
-var apple = new FoodItem('apple (100 grams)', 0, 12, 0.4, 54, 0.12);
+function newFoodItem(id, name, fat, carbs, protein, kcals, price) {
 
-// Create new Food Item
+    foodItems[id] = new FoodItem(id, name, fat, carbs, protein, kcals, price);
 
-var entryName;
-var entryFat;
-var entryCarbs;
-var entryProtein;
-var entryKcals;
-var entryPrice;
+}
+
+newFoodItem(0, 'minced beef', 72, 1.8, 120, 1140, 6.4);
+newFoodItem(1, 'egg', 6.7, 0.9, 7.7, 95, 0.164);
+newFoodItem(2, 'butter', 82.5, 0.7, 0.7, 748, 1.196);
+newFoodItem(3, 'chicken legg', 39, 0, 54, 570, 1.22);
+newFoodItem(4, 'mackerel', 63, 0, 77, 875, 3.32);
+newFoodItem(5, 'apple', 0, 12, 0.4, 54, 0.12);
+
+console.log(foodItems[0].name);
+
+// Create food items
 
 function createFoodItem() {
     document.getElementById("createFoodItem").classList.remove('hidden');
@@ -49,24 +54,33 @@ function createFoodItem() {
 
 function createItem() {
 
-    entryName = document.getElementById('name').value
-    entryFat = document.getElementById('fat').value
-    entryCarbs = document.getElementById('carbs').value
-    entryProtein = document.getElementById('protein').value
-    entryKcals = document.getElementById('kcals').value
-    entryPrice = document.getElementById('price').value
+    var entryName = document.getElementById('name').value;
+    var entryFat = Number(document.getElementById('fat').value);
+    var entryCarbs = Number(document.getElementById('carbs').value);
+    var entryProtein = Number(document.getElementById('protein').value);
+    var entryKcals = Number(document.getElementById('kcals').value);
+    var entryPrice = Number(document.getElementById('price').value);
 
-    var newFoodItem = document.createElement('div');
-    newFoodItem.innerHTML = "&bull; " + entryName;
-    newFoodItem.setAttribute("class", "FoodItem");
-    document.getElementById("test").appendChild(newFoodItem);
-    newFoodItem.setAttribute("id", entryName);
+
+    newFoodItem(id, entryName, entryFat, entryCarbs, entryProtein, entryKcals, entryPrice);
+    console.log(foodItems);
+
+
+    var newFoodItem2 = document.createElement('div');
+    newFoodItem2.innerHTML = "&bull; " + entryName;
+    newFoodItem2.setAttribute("class", "FoodItem");
+    document.getElementById("test").appendChild(newFoodItem2);
+    newFoodItem2.setAttribute("id", entryName);
 
     var newFoodItemAddButton = document.createElement('button');
     newFoodItemAddButton.innerHTML = '+';
     newFoodItemAddButton.value = entryName;
     newFoodItemAddButton.setAttribute("class", "btn btn-info add");
-    newFoodItemAddButton.onclick = "addFoodItemToList(this.value)";
+
+    newFoodItemAddButton.onclick = function() {
+        addFoodItemToList(this.value);
+    };
+
     document.getElementById(entryName).appendChild(newFoodItemAddButton);
 
 }
@@ -76,11 +90,13 @@ function cancel() {
     document.getElementById("createFoodItem").classList.add('hidden');
 }
 
+
 // Add and remove Food Items
 
 function addFoodItemToList(value) {
-    var food = eval(value);
-    valueRemove = value;
+    console.log(value);
+
+    var food = foodItems[value];
 
     var foodItem = document.createElement('div');
     foodItem.innerHTML = food.name + "<br />" + "fat: " + food.fat + " grams carbs: " + food.carbs + " grams protein: " + food.protein + " grams kcals: " + food.kcals + " costs: € " + food.price + "<br />";
