5080b93ec64827e299cef2dc483025b97dc12327
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-06-26 10:37:46 +0200

    Added delete, edit and add icons.

--- index.php
+++ index.php
@@ -60,6 +60,8 @@ if (isset($_POST['create_button'])) {
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
 </head>
 
 <body>
@@ -165,14 +167,15 @@ if (isset($_POST['create_button'])) {
 
                             echo "<tr>";
 
-                            echo "<td style='width:80%'>{$row['fname']}</td>";
-                            echo "<td style='width:10%'><a rel='".$row['id']."' class='delete-this' href='javascript:void(0)'><i style='font-size:24px' class='fa'>&#xf014;</i></a></td>";
-                            echo "<td style='width:10%'><input type='button' class='btn btn-danger add' value='+'></td>";
-                
+                            echo "<td style='width:60%'>{$row['fname']}</td>";
+                            echo "<td style='width:15%'><a rel='".$row['id']."' class='delete-this' href='javascript:void(0)'><i class='material-icons'>delete_outline</i></a></td>";
+                            echo "<td style='width:15%'><a rel='".$row['id']."' class='edit-this' href='javascript:void(0)'><i class='material-icons'>edit</i></a></td>";
+                            echo "<td style='width:10%'><a rel='".$row['id']."' class='add-this' href='javascript:void(0)'><i class='material-icons'>add_circle_outline</i></a></td>";
+
                             echo "</tr>";
                             
 
-
+                            
                         }
                     }
                     $con->close();
