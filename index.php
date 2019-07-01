ed89682c3b038b587f9874a06da35a5da47023b7
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-07-01 17:56:15 +0200

    Added navigation icons to the date secion.

--- index.php
+++ index.php
@@ -56,11 +56,12 @@ if (isset($_POST['create_button'])) {
     <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/css?family=Stylish&display=swap" rel="stylesheet">
     
+    
     <script type="text/javascript" src="javascript.js"></script>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://kit.fontawesome.com/ad07c4a8ed.js"></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
 </head>
@@ -188,7 +189,7 @@ if (isset($_POST['create_button'])) {
 
         </div>
         <div class="grid-item">
-            <h5>Date</h5>
+        <i class="fa fa-angle-left"></i></i><h5 class="date">Date</h5><i class="fa fa-angle-right"></i>
             <hr>
 
             <div id="listToday"></div>
