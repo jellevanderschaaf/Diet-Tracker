0b37b8023669ce46a4db762a9d2bfe6fb76a8b19
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-07-09 20:42:34 +0200

    Played around with different font sizes and weights.

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if (isset($_POST['create_button'])) {
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     
-    <link href="https://fonts.googleapis.com/css?family=Roboto|Stylish&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Stylish&display=swap" rel="stylesheet">
     
     
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
