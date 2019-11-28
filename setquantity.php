f2574b3de50ebd62f6feba97d0d59f5083481cc1
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-11-28 21:26:30 +0100

    Seperate sql calculations for grams and piece.

--- setquantity.php
+++ setquantity.php
@@ -12,14 +12,17 @@ if(isset($_POST['setthisquantity'])) {
     $query = "UPDATE food_items_date SET quantity = '$quantity' WHERE id = $id ";
     $result_set = mysqli_query($con, $query);
 
-    $queryThree = "UPDATE food_items_date SET totalprice = price * quantity";
+    $queryThree = "UPDATE food_items_date SET totalprice = price * quantity WHERE list = 'piece'";
     $result_setThree = mysqli_query($con, $queryThree);
+
+    $queryFour = "UPDATE food_items_date SET totalprice = price / 100 * quantity WHERE list = 'grams'";
+    $result_setFour = mysqli_query($con, $queryFour);
 
     $queryTwo = "SELECT * FROM food_items_date WHERE id = $id";
     $result_setTwo = mysqli_query($con, $queryTwo);
     $row = mysqli_fetch_array($result_setTwo);
 
-    if(!$result_set || !$result_setTwo || !$result_setThree) {
+    if(!$result_set || !$result_setTwo || !$result_setThree || !$result_setFour) {
 
         die("QUERY FAILED" . mysqli_error($con));
         
